4949119055c89b81ad5ebba4d2eac863738af604
Arkadiy <mefrreex@gmail.com>
2025-08-09 17:06:43 +0400

    feat: added event listeners

--- src/main/java/com/luminiadev/lumi/ExamplePlugin.java
+++ src/main/java/com/luminiadev/lumi/ExamplePlugin.java
@@ -1,14 +1,17 @@
 package com.luminiadev.lumi;
 
 import cn.nukkit.block.Block;
+import cn.nukkit.event.player.PlayerJumpEvent;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.enchantment.Enchantment;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
 import com.luminiadev.lumi.command.RandomEffectsCommand;
 import com.luminiadev.lumi.command.SimplePluginCommand;
 import com.luminiadev.lumi.customblock.BlockCustomExample;
 import com.luminiadev.lumi.customenchantment.EnchantmentCustomExample;
 import com.luminiadev.lumi.customitem.ItemCustomExample;
+import com.luminiadev.lumi.listener.EventListener;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -31,6 +34,13 @@ public class ExamplePlugin extends PluginBase {
 
         this.getServer().getCommandMap().register("ExamplePlugin", new RandomEffectsCommand(this));
         this.getServer().getCommandMap().register("ExamplePlugin", new SimplePluginCommand(this));
+
+        // Registering non-reflection event
+        this.getServer().getPluginManager().subscribeEvent(PlayerJumpEvent.class, event -> {
+            event.getPlayer().sendActionBar(TextFormat.GREEN + "You jumped!");
+        }, this);
+        // Registering reflection events
+        this.getServer().getPluginManager().registerEvents(new EventListener(), this);
     }
 
     @Override
